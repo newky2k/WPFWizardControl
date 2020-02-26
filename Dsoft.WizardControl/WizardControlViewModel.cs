1c6a1f5f04210bd2018efeebfe6fc4d44dabfa77
Dave Humphreys <dave@none.com>
2020-02-26 14:31:20 +0000

    Fixed issues with binding errors on CompleteEnabled and CancelEnabled
    Fixed issue with the SetButtonVisibility functionality when mixing All and specific buttons

--- Dsoft.WizardControl/WizardControlViewModel.cs
+++ Dsoft.WizardControl/WizardControlViewModel.cs
@@ -125,6 +125,9 @@ namespace Dsoft.WizardControl.WPF
         {
             get
             {
+                if (Pages.Count == 0 || ActivePages.Count == 0)
+                    return false;
+
                 switch (_currentStage)
                 {
                     case WizardStage.Working:
@@ -211,6 +214,9 @@ namespace Dsoft.WizardControl.WPF
         {
             get
             {
+                if (Pages.Count == 0 || ActivePages.Count == 0)
+                    return false;
+
                 if (ProcessMode == ProcessMode.None)
                 {
                     if (SelectedIndex == ActivePages.Max(x => x.Key))
@@ -233,6 +239,9 @@ namespace Dsoft.WizardControl.WPF
         {
             get
             {
+                if (Pages.Count == 0 || ActivePages.Count == 0)
+                    return false;
+
                 if (ProcessMode == ProcessMode.None)
                 {
                     if (SelectedIndex == ActivePages.Max(x => x.Key))
@@ -865,12 +874,27 @@ namespace Dsoft.WizardControl.WPF
             }
             else
             {
-                var realVisibilty = (visibility == WizardButtonVisibility.Visible) ? Visibility.Visible : Visibility.Collapsed;
-                foreach (var button in buttons)
+                //set the default hidden mode to collapsed(so that the buttons bunch up)
+                var hiddenMode = Visibility.Collapsed;
+
+                //create a local copy of the buttons array
+                var localButtons = new List<WizardButtons>(buttons);
+
+                //if the buttons array conatains WizardButtons.All, rebuild the localbuttons variable all the buttons.  TODO:// This could be update to loop through the Enum instead of being hardcoded
+                if (buttons.Contains(WizardButtons.All))
+                {
+                    localButtons = new List<WizardButtons>() { WizardButtons.Cancel, WizardButtons.Complete, WizardButtons.Next, WizardButtons.Previous, WizardButtons.Process };
+
+                    //if we are explicitly hiding all the buttons then set then use hidden instead of collapsed
+                    hiddenMode = Visibility.Hidden;
+                }
+
+                var realVisibilty = (visibility == WizardButtonVisibility.Visible) ? Visibility.Visible : hiddenMode;
+
+                //loop through all the buttons
+                foreach (var button in localButtons)
                 {
                     //if all buttons use hide not collapsed when hiding all buttons
-                    realVisibilty = (button == WizardButtons.All && visibility == WizardButtonVisibility.Hidden) ? Visibility.Hidden : realVisibilty;
-
                     if (_buttonVisibility.ContainsKey(button))
                     {
                         _buttonVisibility[button] = realVisibilty;
